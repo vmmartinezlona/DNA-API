5048f4bf96ed2fc21d36e75c75c13eb9b81b2a16
Montserrat Lona <montserrat.l@scopicsoftware.com>
2020-11-09 16:44:10 -0600

    check vertical mutation

--- util/dna.ts
+++ util/dna.ts
@@ -5,9 +5,23 @@ async function isNitrogenousBase(dna: [string]) {
 
 async function hasMutation(dna: [string]) {
   const reg = new RegExp("[Aa]{4,}|[Tt]{4,}|[Cc]{4,}|[Gg]{4,}")
-  if (dna.every(e => reg.test(e)) // horizontal
+  if (dna.every(e => reg.test(e)) || // horizontal
+    checkVerticalSecuences(dna, reg) // vertical
     ) {
     return true
+  }
+  return false
+}
+
+function checkVerticalSecuences(dna: [string], reg: RegExp) {
+  for(let i = 0; i <= dna.length; i++) {
+    let verticalString = '' 
+    for (const secuence of dna) {
+      verticalString += secuence[i]
+    }
+    if (reg.test(verticalString)) {
+      return true
+    }
   }
   return false
 }
